cc1871b99f580cae67c904cd144f77ba1c110132
aflatness <aflatness@gmail.com>
2021-02-17 15:04:35 -0700

    json-server set up. Router.render set to change date format. Front end set up to show history, able to search for specif query, and by year.

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
+const years = [];
 
+for(let i = -300; i < 2013; i++ ) {
+  years.push(i);
+}
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [year, setYear] = useState('');
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    axios.get(`/events?_page=${page}${year ? `&date=${year}` : ''}${search ? `&q=${search}` : ''}`)
+      .then(({ data }) => setData(data.body))
+      .catch(console.log);
+  }, [page, year, search]);
+
+  console.log(data, year, search)
+  return (
+    <div>
+      <label>Year to search: {''}
+        <select value={year} onChange={(e) => setYear(e.target.value)} >
+          {years.map(v => <option value={v}>{v}</option>)}
+        </select>
+        <button onClick={() => setYear('')} >Clear year query</button>
+      </label>
+      <br/>
+      <br/>
+      <label>Search: {''}
+        <input value={search} onChange={(e) => setSearch(e.target.value)} />
+      </label>
+      <br/>
+      {data.length ? data.map(d => (
+        <ul>
+          <li>{d.date}</li>
+          <li>{d.description}</li>
+          <li>{d['category2']}</li>
+        </ul>
+        )
+      ) : null}
+      <ReactPaginate onPageChange={(d) => setPage(d.selected + 1)} />
+    </div>
+  )
+}
+
+export default App;
---
+++ challenge_1/server.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./data/db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+router.render = (req, res) => {
+  res.jsonp({
+    body: res.locals.data.map(({ date, description, category2 }) => {
+        date = date.replace(/^\-(.*)/, '$1 B.C.').replace(/(\d+)\/(\d+)\/(\d+)/, '$1')
+        return {date, description, category2};
+      })
+  })
+}
+
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
