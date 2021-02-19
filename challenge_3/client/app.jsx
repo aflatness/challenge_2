f4695c0fb6da98a9b53a9869fbca8dadb17b7c0f
aflatness <aflatness@gmail.com>
2021-02-19 11:23:44 -0700

    Bowling set to determine without spares or strikes. Game can reset

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,62 @@
+import React, { useState, useEffect } from 'react';
 
 
+const App = () => {
+  const [frame, setFrame] = useState(1);
+  const [roll, setRoll] = useState(1);
+  const [score, setScore] = useState([]);
+  const [prevScores, setPrevScores] = useState([]);
+  const [totalScore, setTotalScore] = useState(0);
+  const [pins, setPins] = useState(Array(11).fill(''));
+
+  const nextFrame = () => {
+    setRoll(1);
+    setFrame(frame + 1);
+    setPins(Array(11).fill(''));
+  }
+
+  const submitScore = () => {
+    if (score === 10) {
+      nextFrame();
+      setTotalScore(totalScore + (prevScores[0] || 0) + (prevScores[1] || 0));
+      setPrevScores([prevScores[1], score]);
+    } else if (roll === 2) {
+      nextFrame();
+      setTotalScore(totalScore + prevScores[1] + score);
+      setPrevScores([prevScores[1], score]);
+    } else {
+      setPins(Array(11 - score).fill(''));
+      setPrevScores([prevScores[1], score]);
+      setRoll(2);
+    }
+  }
+
+  const reset = () => {
+    setFrame(1);
+    setRoll(1);
+    setScore([]);
+    setPrevScores([]);
+    setTotalScore(0);
+    setPins(Array(11).fill(''));
+  }
+
+  console.log(frame, score, prevScores, totalScore)
+  return frame <= 10 ? ( <div>
+      <div>Frame: {frame}</div>
+      <div>Total score: {totalScore}</div>
+      <br />
+      <div>Select a score for your bowl: {''}
+        <select value={score} onChange={scoreChange}>
+          {pins.map((v, i) => <option>{i}</option>)}
+        </select> {''}
+        <button onClick={submitScore}>Roll!</button>
+      </div>
+    </div> )
+    : (
+    <div>
+      <h2>You bowled a {totalScore}</h2>
+      <button onClick={reset}>New game</button>
+    </div> )
+}
+
+export default App;
---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client', 'index.js');
+const OUT_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  entry: SRC_DIR,
+  output: {
+    path: OUT_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
